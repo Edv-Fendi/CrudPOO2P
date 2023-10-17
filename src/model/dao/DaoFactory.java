2dc4dba38ba0e834b966634c3e53dc03efbd686d
Edvaldo Fendi <edvaldo.defendi@gmail.com>
2023-10-17 20:05:12 -0300

    Initial commit

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,12 @@
+package model.dao;
+
+public class DaoFactory {
+
+	/*public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}*/
+}
