4d6af48584892b5c69358533a790e4107cfb09c8
Gabriel-Xander <gabrielantxr@gmail.com>
2023-10-31 21:00:57 -0300

    att2

---
+++ src/application/Funcoes.java
@@ -0,0 +1,83 @@
+package application;
+import model.entities.Porte;
+import model.dao.DaoFactory;
+import model.dao.PorteDao;
+
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Funcoes {
+    // eixo
+    static void updatePorte(){
+        Scanner sc = new Scanner(System.in);
+        PorteDao porteDao = DaoFactory.createPorteDao();
+
+        System.out.println("\n=== TEST 4: update =======");
+        System.out.println("informe o id que deseja atualizar");
+        Porte porte = porteDao.findById(sc.nextInt());
+        System.out.println("digite o codigo:");
+        int cod= Integer.parseInt(sc.next());
+        System.out.println("digite a descricao:");
+        sc.nextLine();// para limpar o buffer e liberar a proxima pergunta
+        String desc= sc.nextLine();
+        porte.setId_Porte(cod);
+        porte.setNome(desc);
+        porteDao.update(porte);
+        System.out.println("Update completed");
+        sc.close();
+
+    }
+
+    static void findIdPorte(){
+        Scanner sc = new Scanner(System.in);
+
+        PorteDao porteDao = DaoFactory.createPorteDao();
+
+        System.out.println("=== TEST 1: findById =======");
+        System.out.println("informe o id a ser exibido");
+        Porte dep = porteDao.findById(sc.nextInt());
+        System.out.println(dep);
+        sc.close();
+
+    }
+
+    static void findAllPorte(){
+        PorteDao porteDao = DaoFactory.createPorteDao();
+
+        System.out.println("\n=== TEST 2: findAll =======");
+        List<Porte> list = porteDao.findAll();
+        for (Porte d : list) {
+            System.out.println(d);
+        }
+
+    }
+
+    static void insertPorte(){
+        Scanner sc = new Scanner(System.in);
+
+        PorteDao porteDao = DaoFactory.createPorteDao();
+        System.out.println("\n=== TEST 3: insert =======");
+        System.out.println("digite o codigo");
+        int cod= Integer.parseInt(sc.next());
+        System.out.println("digite a descricao");
+        String desc= sc.nextLine();
+        Porte newPorte = new Porte(cod,desc);
+        porteDao.insert(newPorte);
+        System.out.println("Inserted! New id: " + newPorte.getId_Porte());
+        sc.close();
+    }
+
+    static void deletePorte(){
+        Scanner sc = new Scanner(System.in);
+
+        PorteDao porteDao = DaoFactory.createPorteDao();
+
+        System.out.println("\n=== TEST 5: delete =======");
+        System.out.print("Enter id for delete test: ");
+        int cod= Integer.parseInt(sc.next());
+        porteDao.deleteById(cod);
+        System.out.println("Delete completed");
+        sc.close();
+    }
+}
